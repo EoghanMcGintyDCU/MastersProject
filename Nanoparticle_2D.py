2a1f9babfa6ee9707442001945729c084592794d
Eoghan McGinty <eoghan.mcginty3@mail.dcu.ie>
2022-07-09 17:10:11 +0100

    Latest working scripts w & w/o flux regions for 2 NPs hdf5 working! Output E field image per timestep

--- Nanoparticle_2D.py
+++ Nanoparticle_2D.py
@@ -5,47 +5,9 @@ matplotlib.use('agg')
 import matplotlib.pyplot as plt
 import PyMieScatt as ps
 import sys
+from material import Au_JC_visible
 
-um_scale = 0.1 # 100nm
-eV_um_scale = um_scale/1.23984193
-
-metal_range = mp.FreqRange(min=um_scale/6.1992, max=um_scale/.24797)
-
-Au_plasma_frq = 9.03*eV_um_scale
-Au_f0 = 0.760
-Au_frq0 = 1e-10
-Au_gam0 = 0.053*eV_um_scale
-Au_sig0 = Au_f0*Au_plasma_frq**2/Au_frq0**2
-Au_f1 = 0.024
-Au_frq1 = 0.415*eV_um_scale      # 2.988 μm
-Au_gam1 = 0.241*eV_um_scale
-Au_sig1 = Au_f1*Au_plasma_frq**2/Au_frq1**2
-Au_f2 = 0.010
-Au_frq2 = 0.830*eV_um_scale      # 1.494 μm
-Au_gam2 = 0.345*eV_um_scale
-Au_sig2 = Au_f2*Au_plasma_frq**2/Au_frq2**2
-Au_f3 = 0.071
-Au_frq3 = 2.969*eV_um_scale      # 0.418 μm
-Au_gam3 = 0.870*eV_um_scale
-Au_sig3 = Au_f3*Au_plasma_frq**2/Au_frq3**2
-Au_f4 = 0.601
-Au_frq4 = 4.304*eV_um_scale      # 0.288 μm
-Au_gam4 = 2.494*eV_um_scale
-Au_sig4 = Au_f4*Au_plasma_frq**2/Au_frq4**2
-Au_f5 = 4.384
-Au_frq5 = 13.32*eV_um_scale      # 0.093 μm
-Au_gam5 = 2.214*eV_um_scale
-Au_sig5 = Au_f5*Au_plasma_frq**2/Au_frq5**2
-
-Au_susc = [mp.DrudeSusceptibility(frequency=Au_frq0, gamma=Au_gam0, sigma=Au_sig0),
-           mp.LorentzianSusceptibility(frequency=Au_frq1, gamma=Au_gam1, sigma=Au_sig1),
-           mp.LorentzianSusceptibility(frequency=Au_frq2, gamma=Au_gam2, sigma=Au_sig2),
-           mp.LorentzianSusceptibility(frequency=Au_frq3, gamma=Au_gam3, sigma=Au_sig3),
-           mp.LorentzianSusceptibility(frequency=Au_frq4, gamma=Au_gam4, sigma=Au_sig4),
-           mp.LorentzianSusceptibility(frequency=Au_frq5, gamma=Au_gam5, sigma=Au_sig5)]
-
-Au = mp.Medium(epsilon=1.0, E_susceptibilities=Au_susc, valid_freq_range=metal_range)  
-Au.from_um_factor = um_scale     
+um_scale = 0.1
 
 r = float(sys.argv[1]) # radius of sphere                                                                                                                                    
 
@@ -107,7 +69,7 @@ box_x1_flux0 = mp.get_fluxes(box_x1)
 sim.reset_meep()
 
 n_sphere = 2.0
-geometry = [mp.Sphere(material=Au, #mp.Medium(index=n_sphere),
+geometry = [mp.Sphere(material=Au_JC_visible,
                       center=mp.Vector3(),
                       radius=r)]
 
@@ -131,15 +93,7 @@ sim.load_minus_flux_data(box_x2, box_x2_data)
 sim.load_minus_flux_data(box_y1, box_y1_data)
 sim.load_minus_flux_data(box_y2, box_y2_data)
 
-
-animate = mp.Animate2D(sim,
-                       fields=mp.Ez,
-                       output_plane=mp.Volume(center=mp.Vector3(0,0),size=mp.Vector3(2*(r+0.1*dpad),2*(r+0.1*dpad))),
-                       field_parameters={'alpha':0.8, 'cmap':'RdBu', 'interpolation':'none'},
-                       eps_parameters={'interpolation':'spline36', 'cmap':'binary','alpha':0.5, 'contour':True, 'contour_linewidth':1, 'frequency':None, 'resolution':None})
-
-sim.run(#mp.to_appended("ez-slice", mp.output_efield_z), # need to get working
-        mp.at_every(1,animate),
+sim.run(#mp.to_appended("ez-slice", mp.output_efield_z),
         until_after_sources=100)
 
 box_x1_flux = mp.get_fluxes(box_x1)
@@ -164,7 +118,6 @@ normalized_scatt_eff_meep = (scatt_eff_meep - scatt_eff_meep.min())/ (scatt_eff_
 normalized_scatt_eff_theory = (scatt_eff_theory - scatt_eff_theory.min())/ (scatt_eff_theory.max() - scatt_eff_theory.min())
 
 if mp.am_master():    
-    animate.to_gif(fps=1E9, filename="./gifs/NanoparticleScattering2D_Au_R{}nm.gif".format(r*100))
     plt.figure(dpi=150)
     plt.plot(1/np.asarray(freqs),normalized_scatt_eff_meep,'bo-',label='Meep') #2*np.pi*r* 
     plt.plot(1/np.asarray(freqs),normalized_scatt_eff_theory,'ro-',label='theory')
